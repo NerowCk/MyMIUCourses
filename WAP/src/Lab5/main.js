cd9bf3c0bbd3bb1f95f8ef2fd4cc86e767a4af80
unknown <cherif201400007025@gmail.com>
2022-06-02 09:38:50 -0500

    lab5

--- WAP/src/Lab5/main.js
+++ WAP/src/Lab5/main.js
@@ -33,4 +33,18 @@ function main (){
     printResults('13. printFibo(10,0,1)',  printFibo(10, 0, 1));
 }
 
+function startClock() {
+    setInterval(ticking, 1000);
+}
+
+function ticking() {
+    let date = new Date();
+    // document.getElementById("year").innerHTML = date.getFullYear();
+    // document.getElementById("month").innerHTML = date.getMonth() + 1;
+    // document.getElementById("date").innerHTML = date.getDate();
+    // document.getElementById("hour").innerHTML = date.getHours();
+    // document.getElementById("minute").innerHTML = date.getMinutes();
+    // document.getElementById("seconds").innerHTML = date.getSeconds();
+}
+(startClock())
 console.log(main())
